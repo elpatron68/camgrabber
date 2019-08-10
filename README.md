125127caa63eed56fcf82d1fcf3dceec21ae1ef4
Markus <m.busche@gmail.com>
2019-08-10 16:30:58 +0200

    Get wetaher information

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Saves images from url in a specific intervall and renders a daily time lapse vid
 
 ## Requirements
 
-- Python3
+- Python >= 3.6
 - ffmepg
 - Graphviz
 
-## Installation
+## Installation (Debian/Ubuntu Linux)
 
 `sudo apt-get update && sudo apt-get install -y ffmpeg graphviz python3 python3-venv`
 `python3 -m venv venv`
@@ -20,15 +20,16 @@ Saves images from url in a specific intervall and renders a daily time lapse vid
 
 Edit `main.py` and change `URL`, `INTERVAL` and `FILENAME` to your needs.
 
-## Manual start
+### Manual start
 
 `source venv/bin/activate`
 `python main.py`
 
-## Run permanently as Supervisor service
+### Run permanently as Supervisor service
 
 - Install _Supervisor_: `sudo apt-get install -y supervisor`
-- Edit the configuration file `sudo nano /etc/supervisor/conf.d/camgrabber.conf`:
+- Edit the configuration file:
+    `sudo nano /etc/supervisor/conf.d/camgrabber.conf`
     ```
     [program:camgrabber]
     directory=/home/ubuntu/camgrabber
@@ -43,3 +44,17 @@ Edit `main.py` and change `URL`, `INTERVAL` and `FILENAME` to your needs.
     (Change `directory`, `command` and `user`)
 - Reread and update the configuration: `sudo supervisorctl reread && sudo supervisorctl update`
 - Check status: `sudo supervisorctl status`
+
+If any problems occur, http://www.onurguzel.com/supervisord-restarting-and-reloading/ might help.
+
+## License & Copyright
+
+*MIT License*
+
+(c) 2019 Markus Busche, elpatron@mailbox.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import os
 import time
 import urllib.request
+import requests
 from datetime import date
+from astral import Astral
 import ffmpeg
 
 URL = 'https://www.yacht-club-norden.de/MOBOTIX/nu.jpg'
 INTERVAL = 15
-FILENAME = "ycn-%i.jpg"
+FILENAME = 'ycn-%i.jpg'
+OPENWEATEHR_ID = '2862135'
+CITY_NAME = 'Norden'
+OPENWEATHER_APIKEY = '6ea7a73741212ae93cb6231852f9f7d0'
 
 
 def getimages(day, path):
     try:
         os.mkdir(path)
     except OSError:
-        print(f"Creation of the directory {path} failed")
+        print(f'Creation of the directory {path} failed')
     else:
-        print(f"Successfully created the directory {path}")
+        print(f'Successfully created the directory {path}')
     counter = 0
     while day == date.today():
         f = FILENAME.replace('%i', str(counter).zfill(5))
         fullname = f'{path}/{f}'
         print(f'Saving file: {fullname}')
+        print('Loading weather information')
+        weatherdata = getweather(OPENWEATEHR_ID)
+        print('Inserting weather into image')
+        insertdata(fullname, weatherdata)
         urllib.request.urlretrieve(URL, fullname)
         counter += 1
         print(f'Sleeping {INTERVAL} seconds...')
@@ -28,7 +37,7 @@ def getimages(day, path):
 
 
 def createtimelapse(day, path):
-    f = FILENAME.replace("%i", day.strftime("%Y%m%d"))
+    f = FILENAME.replace('%i', day.strftime('%Y%m%d'))
     fn, file_extension = os.path.splitext(f)
 
     fullname = f'{path}/{fn}'
@@ -41,16 +50,45 @@ def createtimelapse(day, path):
     )
 
 
+def getweather(location_id):
+    base_url = 'http://api.openweathermap.org/data/2.5/weather?'
+    # api.openweathermap.org/data/2.5/weather?id=2172797
+    complete_url = f'{base_url}appid={OPENWEATHER_APIKEY}&id={location_id}&units=metric'
+    response = requests.get(complete_url)
+    x = response.json()
+    if x['cod'] != '404': 
+        main = x['main'] 
+        current_temperature = main['temp'] 
+        current_pressure = main['pressure'] 
+        wind = x['wind']
+        windspeed = wind['speed']
+        winddirection = wind['deg']
+        return current_temperature, current_pressure, windspeed, winddirection
+
+
+def insertdata(img, data):
+    pass
+
+
 def cleanup(path):
     images = os.listdir(path)
 
     for item in images:
-        if item.endswith(".jpg"):
+        if item.endswith('.jpg'):
             try:
                 os.remove(os.path.join(path, item))
             except:
                 pass
         
+
+def getsun():
+    a = Astral()
+    a.solar_depression = 'civil'
+    city = a[CITY_NAME]
+    sun = city.sun(date=date.today, local=True)
+    dawn = sun['dawn']
+    sunset = sun['sunset']
+    return dawn, sunset
 
 if __name__ == '__main__':
     while 1:
