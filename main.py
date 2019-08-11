803681e5d4c41635c8204c016d1d56d488a73198
Markus <m.busche@gmail.com>
2019-08-11 13:34:32 +0200

    Add logging

--- main.py
+++ main.py
@@ -29,50 +29,60 @@ END_AFTER_SUNDOWN = 1
 
 
 def get_images(day, path):
+    logging.debug(f'Creating directory {path}')
     try:
         os.mkdir(path)
     except OSError:
-        print(f'Creation of the directory {path} failed')
-    else:
-        print(f'Successfully created the directory {path}')
+        logging.warn(f'Creation of the directory {path} failed')
     counter = 0
     indexfile = f'{path}/lastindex.txt'
+    
     if os.path.isfile(indexfile):
+        logging.info(f'Reading last image index from {indexfile}')
         f = open(indexfile)
         counter = int(f.read())
+        logging.info(f'Las image index: {counter}')
+        counter += 1
         f.close()
         try:
+            logging.debug('Delete index file')
             os.remove(indexfile)
         except OSError:
             pass
 
     weathercount = 0
     sun = get_sun()
-    sundawn = datetime.strptime(sun[0], '%Y-%m-%dT%H:%M:%SZ')
-    sundown = datetime.strptime(sun[1], '%Y-%m-%dT%H:%M:%SZ')
-    start = sundawn - timedelta(hours=START_BEFORE_SUNDAWN)
-    end = sundawn + timedelta(hours=END_AFTER_SUNDOWN)
+    sun_dawn = datetime.strptime(sun[0], '%Y-%m-%dT%H:%M:%SZ')
+    logging.debug(f'Sun dawn: {sun_dawn}')
+    sun_down = datetime.strptime(sun[1], '%Y-%m-%dT%H:%M:%SZ')
+    logging.debug(f'Sun down: {sun_down}')
+    start = sun_dawn - timedelta(hours=START_BEFORE_SUNDAWN)
+    logging.debug(f'Start: {start}')
+    end = sun_dawn + timedelta(hours=END_AFTER_SUNDOWN)
+    logging.debug(f'End: {end}')
+
     while day == date.today():
         now = datetime.utcnow()
         if now > start and now < end:
+            logging.info('Loading image')
             f = FILENAME.replace('%i', str(counter).zfill(5))
             fullname = f'{path}/{f}'
+            logging.debug(f'Image file name: {fullname}')
             if weathercount == 0:
-                print('Loading new weather information')
+                logging.info('Loading new weather information')
                 weatherdata = get_weather()
             weathercount += 1
-            print(f'Saving file: {fullname}')
             urllib.request.urlretrieve(URL, fullname)
-            print('Inserting weather into image')
+            logging.info('Inserting weather into image')
             insert_weather_data(fullname, weatherdata)
             save_lastindex(path, counter)
             counter += 1
-            print(f'Sleeping {INTERVAL} seconds...')
             time.sleep(INTERVAL)
             if weathercount > 39:
+                logging.debug('Resetting weather counter to zero')
                 weathercount = 0
         else:
-            print('Breaking loop')
+            logging.info('Time before start or after end, breaking loop')
             break
 
 
@@ -82,14 +92,14 @@ def create_timelapse(day, source_path, dest_path):
     fn, file_extension = os.path.splitext(f)
     fn = path_leaf(fn)
     fullname = f'{dest_path}/{fn}.mp4'
+    logging.info(f'Creating time lapse: {fullname}')
+
     try:
         os.mkdir(dest_path)
     except OSError:
-        print(f'Creation of the directory {dest_path} failed')
-    else:
-        print(f'Successfully created the directory {dest_path}')
+        logging.warn(f'Creation of the directory {dest_path} failed')
 
-    print(f'Rendering images from {source_path} to mp4 video file: {fullname}')
+    logging.info(f'Rendering images from {source_path} to mp4 video file: {fullname}')
     (
         ffmpeg
         .input(f'{source_path}/*{file_extension}', pattern_type='glob', framerate=25)
@@ -102,19 +112,22 @@ def get_weather():
     base_url = 'http://api.openweathermap.org/data/2.5/weather?'
     # api.openweathermap.org/data/2.5/weather?id=2172797
     complete_url = f'{base_url}appid={OPENWEATHER_APIKEY}&id={OPENWEATEHR_ID}&units=metric'
+    logging.info(f'Getting weather information from {complete_url}')
     response = requests.get(complete_url)
     x = response.json()
     if x['cod'] != '404': 
         main = x['main'] 
-        current_temperature = main['temp'] 
+        current_temperature = main['temp']
         current_pressure = main['pressure'] 
         wind = x['wind']
         windspeed = wind['speed']
         winddirection = wind['deg']
+        logging.debug(f'Temp: {current_temperature}, Pressure: {current_pressure}, Wind speed: {windspeed}, Wind direction: {winddirection}')
         return current_temperature, current_pressure, windspeed, winddirection
 
 
 def insert_weather_data(imagefile, data):
+    logging.info(f'Inserting weather information into image {imagefile}')
     img = Image.open(imagefile)
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype(r'MicrosoftSansSerifRegular.ttf', 16)
@@ -137,6 +150,7 @@ def cleanup(path):
         
 
 def get_sun():
+    logging.debug(f'Calculating sun dawn end down for LAT {LAT}, LON {LON}')
     location = api.Topos(LAT, LON)
     ts = api.load.timescale()
     e = api.load('de421.bsp')
@@ -145,7 +159,6 @@ def get_sun():
     t1 = ts.utc(today.year, today.month, today.day, 23)
     t, y = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(e, location))
     sun = t.utc_iso()
-    sun2 = t
     return sun
 
 
@@ -154,7 +167,9 @@ def path_leaf(path):
     return tail or ntpath.basename(head)
 
 def save_lastindex(path, index):
-    f = open(f'{path}/lastindex.txt','w')
+    indexfile = f'{path}/lastindex.txt'
+    logging.debug(f'Saving last image index {index} to {indexfile}')
+    f = open(indexfile, 'w')
     f.write(index)
     f.close
     pass
@@ -177,5 +192,5 @@ if __name__ == '__main__':
                     create_timelapse(today, path, DESTINATION_PATH)
                     cleanup(path)
         else:
-            print('It´s still dark...')
+            print('It´s still dark outside...')
             time.sleep(60)
