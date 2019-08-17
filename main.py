a99b197417c3ad917a1c56c912f666a510117832
Markus <m.busche@gmail.com>
2019-08-17 14:34:40 +0200

    Calcutale average waether values

--- main.py
+++ main.py
@@ -46,6 +46,10 @@ elif CONFIG['general']['loglevel'].lower() == 'warning':
 # Constants
 START_BEFORE_SUNDAWN = int(CONFIG['recording']['start_before_dawn'])
 END_AFTER_SUNDOWN = int(CONFIG['recording']['end_after_sundown'])
+WEATHER_VALUES = 0
+WEATHER_WINDSPEED_AVG = 0
+WEATHER_TEMP_AVG = 0
+WEATHER_PRESSURE_AVG = 0
 
 
 def get_images(day, path):
@@ -180,6 +184,13 @@ def get_weather():
             windspeed = wind['speed']
             winddirection = wind['deg']
             logging.debug(f'Temp: {current_temperature}, Pressure: {current_pressure}, Wind speed: {windspeed}, Wind direction: {winddirection}')
+            
+            # Calcutale average waether values
+            WEATHER_VALUES += 1
+            WEATHER_PRESSURE_AVG = (WEATHER_PRESSURE_AVG + current_pressure) / WEATHER_VALUES
+            WEATHER_TEMP_AVG = (WEATHER_TEMP_AVG + current_temperature) / WEATHER_VALUES
+            WEATHER_WINDSPEED_AVG = (WEATHER_WINDSPEED_AVG + windspeed) / WEATHER_VALUES
+
             return current_temperature, current_pressure, windspeed, winddirection
     except:
         logging.warn(f'Failed to retreive weather data. Response was\n{x}')
