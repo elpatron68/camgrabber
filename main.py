9100cd4959e1db49bf366af26fdd941bb00601f2
Markus <m.busche@gmail.com>
2019-09-06 08:19:15 +0200

    DFeactivate database

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ def get_weather():
                 weatherdata['winddirection'] = winddirection
                 weatherdata['pressure'] = current_pressure
                 weatherdata['temperature'] = current_temperature
-                save_weather_to_db(weatherdata)
+                # save_weather_to_db(weatherdata)
 
         except:
             logging.warn(f'Failed to retreive weather data. Response was\n{x}')
@@ -256,6 +256,7 @@ def save_weather_to_db(data):
         database.update_db(CONFIG['general']['database'], data['tablename'], data)
     except:
         pass
+
 
 def ping_healthchecks(image):
     if CONFIG['general']['enable_healtchecks'].lower() == 'true':
